3460b58af4f2bdf4a9015eecd1a0b946ce28f19f
Aswin Anil <aswin98@gmail.com>
2018-07-26 12:51:02 +0530

    Update sqlexp1.sql

--- sqlexp1.sql
+++ sqlexp1.sql
@@ -3,3 +3,7 @@ Alter table course add column credit int;
 Alter table course drop column credit;
 Alter table course add constraint che check(credit>10);   //select each statement to be executed seperately
 drop table course;
+create table courseoffering(cno int, secno int);
+alter table courseoffering
+ add constraint Pk_constraint
+  primary key(cno,secno)
